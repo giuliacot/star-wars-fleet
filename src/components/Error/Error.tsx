d55e05a9991be0d9b40b9afece791fdb39cf1e07
Giulia Talamonti <giulia.talamonti@credimi.com>
2020-12-28 20:56:42 +0100

    updates Error component

--- src/components/Error/Error.tsx
+++ src/components/Error/Error.tsx
@@ -1,7 +1,7 @@
 import React, { FunctionComponent, ReactNode } from 'react';
 import style from './Error.module.css';
 
-export const Error: FunctionComponent<{ children: ReactNode }> = ({
+export const Error: FunctionComponent<React.HTMLAttributes<HTMLDivElement>> = ({
   children,
 }) => {
   return <h4 className={style.msg}>{children}</h4>;
