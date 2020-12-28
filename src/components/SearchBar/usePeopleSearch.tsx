6346ae3e0ef59101e7f8aaa9e57aa11c81de5472
Giulia Talamonti <giulia.talamonti@credimi.com>
2020-12-28 20:58:12 +0100

    remove old hook

--- src/components/SearchBar/usePeopleSearch.tsx
+++
@@ -1,9 +0,0 @@
-import { useQuery } from 'react-query';
-
-export const usePeopleSearch = ({ value }: { value: string }) => {
-  return useQuery('peoplesearch', () =>
-    fetch(`https://swapi.dev/api/people/?search=${value}`).then((res) =>
-      res.json(),
-    ),
-  );
-};
